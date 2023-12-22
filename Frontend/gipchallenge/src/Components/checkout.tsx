a701d5434f713c1a33f67016ef26adfd07b4e524
Kweryzm <Amessaoudenecontact@gmail.com>
2023-12-22 21:11:27 +0100

    Update checkout.tsx

--- Frontend/gipchallenge/src/Components/checkout.tsx
+++ Frontend/gipchallenge/src/Components/checkout.tsx
@@ -1,4 +1,3 @@
-// CheckoutItem.tsx (Updated)
 import React from 'react';
 
 interface CheckoutItemProps {
@@ -37,63 +36,45 @@ const CheckoutItem: React.FC<CheckoutItemProps> = ({
     };
 
     return (
-        <div className="card mb-3">
-            <div className="card-body d-flex flex-column flex-lg-row align-items-center">
-                <div className="item-image me-lg-3 mb-3 mb-lg-0">
-                    <img src={imageUrl} alt={itemName} className="img-fluid" />
-                </div>
-                <div className="item-details w-100">
-                    <h5 className="card-title mb-2 mb-lg-0">Swaggy store</h5>
-                    <div className="row align-items-center">
-                        <div className="col-md-6 mb-2 mb-md-0">
-                            <p className="card-text mb-1">{itemName}</p>
-                            <h5 className="card-title mb-2 mb-lg-0">{category}</h5>
-                            <div className="d-flex align-items-center">
-                                <span className="me-2">Size:</span>
-                                <span>{size}</span>
-                            </div>
-                        </div>
-                        <div className="col-md-6">
-                            <div className="input-group">
-                                <button
-                                    className="btn btn-outline-secondary"
-                                    type="button"
-                                    onClick={handleDecrease}
-                                    aria-label="Decrease quantity"
-                                >
-                                    -
-                                </button>
-                                <input
-                                    type="number"
-                                    className="form-control text-center"
-                                    value={quantity}
-                                    onChange={(e) => updateQuantity(index, parseInt(e.target.value))}
-                                    aria-label="Quantity"
-                                    aria-valuemin={1}
-                                />
-                                <button
-                                    className="btn btn-outline-secondary"
-                                    type="button"
-                                    onClick={handleIncrease}
-                                    aria-label="Increase quantity"
-                                >
-                                    +
-                                </button>
-                            </div>
-                            <div className="d-flex justify-content-between mt-2">
-                                <span>Price: ${itemPrice.toFixed(2)}</span>
-                                <span>Total: ${totalPrice.toFixed(2)}</span>
-                            </div>
-                            <div className="mt-2">
-                                <button
-                                    className="btn btn-danger"
-                                    onClick={() => removeItem(index)}
-                                    aria-label="Remove item"
-                                >
-                                    Remove
-                                </button>
-                            </div>
-                        </div>
+        <div className="checkout-item-container">
+            <div className="checkout-item">
+                <img src={imageUrl} alt={itemName} className="checkout-item-image" />
+                <div className="checkout-item-details">
+                    <h3 className="checkout-item-name">{itemName}</h3>
+                    <p className="checkout-item-category">{category}</p>
+                    <p className="checkout-item-size">Size: {size}</p>
+                    <div className="checkout-item-actions">
+                        <button
+                            className="checkout-item-decrease"
+                            onClick={handleDecrease}
+                            aria-label="Decrease quantity"
+                        >
+                            -
+                        </button>
+                        <input
+                            type="number"
+                            className="checkout-item-quantity"
+                            value={quantity}
+                            onChange={(e) => updateQuantity(index, parseInt(e.target.value))}
+                            aria-label="Quantity"
+                            aria-valuemin={1}
+                        />
+                        <button
+                            className="checkout-item-increase"
+                            onClick={handleIncrease}
+                            aria-label="Increase quantity"
+                        >
+                            +
+                        </button>
+                        <span className="checkout-item-price">${itemPrice.toFixed(2)}</span>
+                        <span className="checkout-item-total">${totalPrice.toFixed(2)}</span>
+                        <button
+                            className="checkout-item-remove"
+                            onClick={() => removeItem(index)}
+                            aria-label="Remove item"
+                        >
+                            Remove
+                        </button>
                     </div>
                 </div>
             </div>
